3bf7eb5f07e58d607db12ee6e77ec685fe3a868c
Denys Yevdushchenko <a2ka7.dp@gmail.com>
2019-12-11 21:23:28 +0200

    fix tests (#11)

--- projects/carousel-app/src/app/core/components/vertical-navigation/vertical-navigation.component.spec.ts
+++ projects/carousel-app/src/app/core/components/vertical-navigation/vertical-navigation.component.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { VerticalNavigationComponent } from './vertical-navigation.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatButtonModule } from '@angular/material/button';
 
-describe('AppComponent', () => {
+describe('VerticalNavigationComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FlexLayoutModule,
+        MatButtonModule,
       ],
       declarations: [
         VerticalNavigationComponent
@@ -14,22 +18,9 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
+  it('should be created', () => {
     const fixture = TestBed.createComponent(VerticalNavigationComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'carousel-app'`, () => {
-    const fixture = TestBed.createComponent(VerticalNavigationComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('carousel-app');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(VerticalNavigationComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('carousel-app app is running!');
   });
 });
